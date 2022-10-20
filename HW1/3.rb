e9828cf51b411e55fac1b5f3527767dddd700f4b
Serhii Dratovanyi <boniole@outlook.com>
2022-10-20 22:31:33 +0300

    1-4, 9-12, 40, 77-78, 87-90

--- HW1/3.rb
+++ HW1/3.rb
@@ -1,8 +1,6 @@
 p 'Дан целочисленный массив. Вывести номер первого из тех его элементов, которые удовлетворяют двойному неравенству: A[0] < A[i] < A[-1]. Если таких элементов нет, то вывести [ ].'
-p '[24,19,1,23,45,22,12,19,39,20,51,62,33,44,15,16,17,18,19,100]'
+p 'array = [24,19,1,23,45,22,12,19,39,20,51,62,33,44,15,16,17,18,19,100] result = [] array.each_with_index {|v, i| result.append(i) if array[0] < array[i] and array[i] < array[-1] and result.size == 0} p result'
 array = [24,19,1,23,45,22,12,19,39,20,51,62,33,44,15,16,17,18,19,100]
 result = []
-array.each_with_index {|v, i| result.append(v) if array[0] < array[i] and array[i] < array[-1]}
-result.append('[ ]')
-p result.first
-p '--------------'
+array.each_with_index {|v, i| result.append(i) if array[0] < array[i] and array[i] < array[-1] and result.size == 0}
+p result
