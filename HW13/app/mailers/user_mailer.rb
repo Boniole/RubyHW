41f35f81d6045379d63c632074747b31ba5f3cf5
Serhii Dratovanyi <boniole@outlook.com>
2023-01-29 17:28:42 +0200

    HW13 modify image and fix bugs and filename

--- HW13/app/mailers/user_mailer.rb
+++ HW13/app/mailers/user_mailer.rb
@@ -2,9 +2,9 @@ class UserMailer < ApplicationMailer
   def send_order(user, order)
     @user = user
     @order = order
-    order.cart.lineitems.each_with_index do |line_item, index|
+    order.cart.lineitems.each do |line_item|
       image = line_item.product.image
-      attachments.inline[image.name+"_"+index.to_s+"_"+image.blob.filename.to_s] = image.download
+      attachments.inline[line_item.product.name+"_"+image.blob.filename.to_s] = image.download
     end
     mail( to: @user.email, subject: "Your order in CatShop")
   end
